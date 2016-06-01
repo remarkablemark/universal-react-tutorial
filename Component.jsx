86f61be7b7e349c41fd914cf1cc2dfea32ed0f32
Mark <mark@remarkablemark.org>
2016-05-31 20:38:24 -0400

    Create `style.css` and refactor Component with the stylesheet
    - Create `public/style.css`
    - Refactor `Component.jsx` so it renders valid HTML
    - Load stylesheet in head

--- Component.jsx
+++ Component.jsx
@@ -6,11 +6,19 @@ module.exports = React.createClass({
     },
     render: function() {
         return (
-            <div>
-                <h1>Hello World!</h1>
-                <p>Isn't server-side rendering remarkable?</p>
-                <button onClick={this._handleClick}>Click Me</button>
-            </div>
+            <html>
+                <head>
+                    <title>Universal App with React</title>
+                    <link rel='stylesheet' href='/style.css' />
+                </head>
+                <body>
+                    <div>
+                        <h1>Hello World!</h1>
+                        <p>Isn't server-side rendering remarkable?</p>
+                        <button onClick={this._handleClick}>Click Me</button>
+                    </div>
+                </body>
+            </html>
         );
     }
 });
