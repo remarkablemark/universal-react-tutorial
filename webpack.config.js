eebb990b0a203ad81fa66f7fcf754cb45d7f7b2b
Mark <mark@remarkablemark.org>
2016-05-31 20:53:51 -0400

    Create `webpack.config.js` and add `webpack` to dependencies
    - The webpack configuration will build the client-side bundle
      to `public` directory
    - Add webpack to dependencies to ensure it's installed
      (we shouldn't assume that it's installed globally)

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './client.js',
+    output: {
+        filename: 'bundle.js',
+        path: 'public'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react']
+                }
+            }
+        ]
+    }
+};
