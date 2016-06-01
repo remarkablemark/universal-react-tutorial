2e38245d18a69f4f1eb4ad5fb4f601f78b902ec6
Mark <mark@remarkablemark.org>
2016-05-31 20:34:25 -0400

    Use `express.static` middleware to serve static files in `public`
    Documentation: expressjs.com/en/starter/static-files.html

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 var Component = require('./Component.jsx');
 
+app.use(express.static('public'));
+
 app.get('/', function(request, response) {
     var html = ReactDOMServer.renderToString(
         React.createElement(Component)
