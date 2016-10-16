b2470c31b24db898443104d8354c5c740d9bf016
Mark <mark@remarkablemark.org>
2016-10-16 13:48:56 -0400

    Set up client-side routing with react-router
    - Replace the component with routes in `client.js`
    - Then in `routes/routes.jsx`, add `createElement()` that merges
      router props with `window.PROPS` if in browser
    
    Todo:
    - Separate or namespace `window.PROPS` so that it doesn't
      override any router props

--- client.js
+++ client.js
@@ -1,9 +1,6 @@
-var React = require('react');
 var ReactDOM = require('react-dom');
-var Component = require('./Component.jsx');
-
-var props = window.PROPS;
+var routes = require('./routes/routes.jsx');
 
 ReactDOM.render(
-    React.createElement(Component, props), document
+    routes, document
 );
--- routes/routes.jsx
+++ routes/routes.jsx
@@ -4,8 +4,14 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var browserHistory = ReactRouter.browserHistory;
 
+if (typeof window === 'object') {
+    function createElement(Component, props) {
+        return <Component {...props} {...window.PROPS} />;
+    }
+}
+
 module.exports = (
-    <Router history={browserHistory}>
+    <Router history={browserHistory} createElement={createElement}>
         <Route path='/' component={require('../Component.jsx')}>
         </Route>
     </Router>
