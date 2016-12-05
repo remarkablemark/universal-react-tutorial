f7b1edf18a20014030ea4e7b5704e68226709a09
Mark <mark@remarkablemark.org>
2016-12-05 15:53:27 -0500

    Update webpack to run babel-loader on `.js` and `.jsx`

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.jsx$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
